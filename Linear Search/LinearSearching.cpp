39d9829432703b8c2beced51b101b65f92551fb7
izzy1-1 <israrqayyum11@gmail.com>
2024-07-23 16:55:29 +0500

    Added C++ Program for Linear Searching

--- Linear Search/LinearSearching.cpp
+++ Linear Search/LinearSearching.cpp
@@ -1,33 +1,38 @@
 #include<iostream>
-  using namespace std;
-  void linearSearch(int *array,int size,int target){
-    int index=-1;
-    for(int i=0;i<size;i++){
-        if(array[i]==target){
-            index=i;
-            break;
+using namespace std;
+
+void linearSearch(int *array, int size, int target) {
+    int index = -1;  // Initialize index to -1 indicating target not found
+    for(int i = 0; i < size; i++) {
+        if(array[i] == target) {
+            index = i;  // Update index if target is found
+            break;  // Exit loop once target is found
         }
     }
-    if(index != -1){
-        cout<<"Element matches in index: "<<index<<endl;
+    if(index != -1) {
+        cout << "Element matches in index: " << index << endl;
+    } else {
+        cout << "Not found the element " << target << " in the array" << endl;
     }
-    else{
-        cout<<"Not found the element "<<target<<" in an array"<<endl;
+}
+
+int main() {
+    int size, element;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    // Dynamically allocate array to allow runtime size determination
+    int *arr = new int[size];
+    cout << "Enter " << size << " elements in the array: ";
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
     }
-  }
-  int main(){
-    int size,element;
-    cout<<"Enter the size of an array: ";
-    cin>>size;
-    //we are using the concept of dynamic array here in which at runtime its size shrinks in memory
-    int *arr= new int[size];
-    cout<<"Enter "<<size<<" elements in an array:";
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-    cout<<"Enter the element you want to search in an array: ";
-    cin>>element;
-    linearSearch(arr,size,element);
-    delete [] arr;
+
+    cout << "Enter the element you want to search in the array: ";
+    cin >> element;
+
+    linearSearch(arr, size, element);
+
+    delete [] arr;  // Free dynamically allocated memory
     return 0;
-  }
+}
